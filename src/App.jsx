7d9d586ec15442798185a36d5cc69d6087804c86
Alok Dubey <adubey@balajeegroup.com>
2025-02-25 18:57:33 +0530

    btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import react from 'react'
-import Display from './component/Display'
+import Wrapper from './component/Wrapper'
+import Screen from './component/Screen'
+import Button from './component/Button'
+import ButtonBox from './component/ButtonBox'
 
+const btnValues=[
+  ["C","+-","%","/"],
+  [7,8,9,"x"],
+  [4,5,6,"-"],
+  [1,2,3,"+"],
+  [0,".","="]
+]
 function App() {
 
   return (
     <>
-      <Display/>
+      <Wrapper>
+        <Screen/>
+        <ButtonBox>
+          {btnValues.map((btn,i)=>(
+            <Button value={btn}/>
+          ))}
+          
+        </ButtonBox>
+      </Wrapper>
     </>
   )
 }
--- src/component/ButtonBox.jsx
+++ src/component/ButtonBox.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Numbers = () => {
+const ButtonBox = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Numbers = () => {
   )
 }
 
-export default Numbers
+export default ButtonBox
